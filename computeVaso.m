0a32f0e1e03a22afb4e1f33d4073a89148586453
Eli Merriam <13526615+elimerriam@users.noreply.github.com>
2021-12-15 09:56:14 -0500

    Add files via upload
    Added capability of dealing with VASO data in which the nulled volume comes first

--- computeVaso.m
+++ computeVaso.m
@@ -27,25 +27,47 @@ for iScan = 1:nScans
   v = viewSet(v, 'curGroup', 'MotionComp');
   tSeries = loadTSeries(v, iScan);
   hdr = viewGet(v, 'niftihdr', iScan);
-  % subset the correct frames
-  bold = tSeries(:,:,:,1:2:end);
-  nulled = tSeries(:,:,:,2:2:end);
-  % load the scan params and nifti header
-  scanParams = viewGet(v, 'scanparams', iScan);
-  % original size
-  [Nx Ny Nz Nt] = size(bold);
-  % upsample bold (forward for half of a TR)
-  [xgrid,ygrid,zgrid,tgrid] = ndgrid(1:Nx, 1:Ny, 1:Nz, 1:0.5:Nt+0.5);
-  boldUp = interpn(bold, xgrid, ygrid, zgrid, tgrid, 'linear');
-  % upsample nulled (backward for half of a TR)
-  [xgrid,ygrid,zgrid,tgrid] = ndgrid(1:Nx, 1:Ny, 1:Nz, 0.5:0.5:Nt);
-  nulledUp = interpn(nulled, xgrid, ygrid, zgrid, tgrid, 'linear');
-
+  
+  boldfirst = [];
+  
+  if boldfirst
+    disp('BOLD before nulled');
+    % subset the correct frames
+    bold = tSeries(:,:,:,1:2:end);
+    nulled = tSeries(:,:,:,2:2:end);
+    % load the scan params and nifti header
+    scanParams = viewGet(v, 'scanparams', iScan);
+    % original size
+    [Nx Ny Nz Nt] = size(bold);
+    % upsample bold (forward for half of a TR)
+    [xgrid,ygrid,zgrid,tgrid] = ndgrid(1:Nx, 1:Ny, 1:Nz, 1:0.5:Nt+0.5);
+    boldUp = interpn(bold, xgrid, ygrid, zgrid, tgrid, 'linear');
+    % upsample nulled (backward for half of a TR)
+    [xgrid,ygrid,zgrid,tgrid] = ndgrid(1:Nx, 1:Ny, 1:Nz, 0.5:0.5:Nt);
+    nulledUp = interpn(nulled, xgrid, ygrid, zgrid, tgrid, 'linear');
+  else
+    disp('Nulled images before BOLD');
+    % subset the correct frames
+    nulled = tSeries(:,:,:,1:2:end);
+    bold = tSeries(:,:,:,2:2:end);
+    % load the scan params and nifti header
+    scanParams = viewGet(v, 'scanparams', iScan);
+    % original size
+    [Nx Ny Nz Nt] = size(bold);
+    % upsample nulled (forward for half of a TR)
+    [xgrid,ygrid,zgrid,tgrid] = ndgrid(1:Nx, 1:Ny, 1:Nz, 1:0.5:Nt+0.5);
+    nulledUp = interpn(nulled, xgrid, ygrid, zgrid, tgrid, 'linear');
+    % upsample bold (backward for half of a TR)
+    [xgrid,ygrid,zgrid,tgrid] = ndgrid(1:Nx, 1:Ny, 1:Nz, 0.5:0.5:Nt);
+    boldUp = interpn(bold, xgrid, ygrid, zgrid, tgrid, 'linear');
+  end    
+  
   % compute vaso
   dims = size(boldUp);  
   boldUp = reshape(boldUp, dims(1)*dims(2)*dims(3), dims(4));
   dims = size(nulledUp);  
   nulledUp = reshape(nulledUp, dims(1)*dims(2)*dims(3), dims(4));
+  
   % loop over frames
   vaso = zeros(size(nulledUp));
   for iFrame = 1:dims(4);
